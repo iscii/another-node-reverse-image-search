5f1f9a23b1811d233c9def6147ffa551bd5e3945
iscii <issac.zheng9869@gmail.com>
2021-07-31 22:34:36 -0400

    features and stuff

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const puppeteer = require('puppeteer');
 
 module.exports = (async (imageUrl, callback) => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
-  const page = await browser.newPage();
-  await page.goto('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(imageUrl));
+	const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+	const page = await browser.newPage();
+	await page.goto('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(imageUrl));
 
-  const images = await page.evaluate(() => {
-    return Array.from(document.body.querySelectorAll('div div a h3')).slice(2).map(e => e.parentNode).map(el => ({ url: el.href, title: el.querySelector('h3').innerHTML }))
-  })
-  callback(images);
-  await browser.close();
+	const images = await page.evaluate(() => {
+		return Array.from(document.body.querySelectorAll('div div a h3')).slice(3) //this queryselector is necessary to get only the individual search result elements
+			.map(e => e.parentNode.parentNode.parentNode.parentNode) //el is the node for the div embodying the two divs that hold url/title and description
+			.map(el => ({
+				res: el.querySelectorAll("div div div span")[4].innerHTML.replace(/\ —\ /g, "").replaceAll(/<\/?span.*?>/g, ""), //removes emdash and spans
+        		desc: el.querySelectorAll("div div div span")[4].innerHTML.includes("</span>") //if res has a date...
+				? el.querySelectorAll("div div div span")[4].parentElement.innerHTML.replaceAll(/(\/?em>|&nbsp;|<\/?span.*>)/g, "").replaceAll(/&amp;/g,"&") //take the div's innerHTML (the desc) and remove the span. else...
+				: el.querySelectorAll("div div div span")[5].innerHTML.replaceAll(/(<\/?em>|&nbsp;)/g,"").replaceAll(/&amp;/g,"&"), //take the regular desc location (span)'s innerHTML. other regexes remove bold tags and some other things
+				title: el.childNodes[0].childNodes[0].childNodes[0].querySelector("h3").innerHTML,
+				url: el.childNodes[0].childNodes[0].childNodes[0].href,
+				imgurl: el.querySelector("g-img img").src,
+			}))
+	})
+	callback(images);
+	await browser.close();
 });
--- test.js
+++ test.js
@@ -4,4 +4,4 @@ const doSomething = (res) => {
   console.log(res)
 }
 
-reverseImageSearch('i.ebayimg.com/00/s/OTAwWDkwMA==/z/3G8AAOSwzoxd80XB/$_83.JPG', doSomething)
+reverseImageSearch('https://cdn.discordapp.com/attachments/494223131683061763/868615421362839583/91403647_p0_master1200.png', doSomething)
