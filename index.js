8930b1ff9c833773ed02a0426ca0c74bd6f6f5a2
iscii <issac.zheng9869@gmail.com>
2021-08-15 20:35:24 -0400

    prevented kill errors & optimized (aka hardcoded) for some new formats

--- index.js
+++ index.js
@@ -6,22 +6,42 @@ module.exports = (async (imageUrl, callback) => {
 	await page.goto('https://www.google.com/searchbyimage?image_url=' + encodeURIComponent(imageUrl));
 	let images;
 	images = await page.evaluate(() => {
-		const span = 5; //this const might be variable; last time it worked with 4, now 5.
-		const desc = 6;
+		let span = 5; //this const might be variable; last time it worked with 4, now 5.
+		const getres = (el) => {
+			const bodyspan = el.querySelectorAll("div div div span");
+			span = 5;
+			
+			if(bodyspan.length < 4){
+				span = 0;
+				return "No Res Found";
+			}
+			if(bodyspan.length < 6){ //if span has less than 5 bodyspans, set span to 3 or 2. else leave it at 5
+				span = bodyspan[2] ? 2 : bodyspan[3] ? 3 : 0
+				if(bodyspan.length < 4 || span == 0){
+					span = 0;
+					return "No Res Found";
+				}
+			}
+			if(bodyspan[span].innerHTML == "Similar"){ //if span's content is "Similar", go to next span. Might have to do this with "Cached" as well.
+				span++;
+			}
+			return bodyspan[span].innerHTML.replace(/\ —\ /g, "").replaceAll(/<\/?span.*?>/g, ""); //removes emdash and spans
+		}
 		//what are the dudes at google doing? the html is inconsistent as heck.
 		const getdesc = (el) => {
 			const bodyspan = el.querySelectorAll("div div div span");
+			if(span == 0) return "No Desc Found";
 			//if res has a span, it has a date.
 			if (bodyspan[span].innerHTML.includes("</span>")) {
 				//if res has a span amd if the div has more than 2 spans, desc has its own span. .match...length finds count of span end tags in the div's innerhtml.
 				if (bodyspan[span].parentElement.innerHTML.match(/<\/span>/g || []).length > 2) { 
-					return bodyspan[desc].innerHTML.replaceAll(/(\/?em>|&nbsp;)/g, "").replaceAll(/&amp;/g, "&") //the desc span is at index 6 of bodyspan. removes bold tags and certain common entities.
+					return bodyspan[span+1].innerHTML.replaceAll(/(\/?em>|&nbsp;)/g, "").replaceAll(/&amp;/g, "&") //the desc span is at index 6 of bodyspan. removes bold tags and certain common entities.
 				}
 			}
 			else {
 				//if res has a span and the div has more than 1 span, desc has its own span.
 				if (bodyspan[span].parentElement.innerHTML.match(/<\/span>/g || []).length > 1) {
-					return bodyspan[desc].innerHTML.replaceAll(/(<\/?em>|&nbsp;)/g, "").replaceAll(/&amp;/g, "&") //here, the desc span is at index 5 of bodyspan. removes bold tags and certain common entities.
+					return bodyspan[span+1].innerHTML.replaceAll(/(<\/?em>|&nbsp;)/g, "").replaceAll(/&amp;/g, "&") //here, the desc span is at index 5 of bodyspan. removes bold tags and certain common entities.
 				}
 			}
 			return bodyspan[span].parentElement.innerHTML.replaceAll(/(\/?em>|&nbsp;|<\/?span.*>)/g, "").replaceAll(/&amp;/g, "&") //the desc doesn't have its own span and exists in the div's innerHTML. takes the div's innerHTML and removes the res span.
@@ -29,13 +49,13 @@ module.exports = (async (imageUrl, callback) => {
 		return Array.from(document.body.querySelectorAll('div div a h3')).slice(3) //this queryselector is necessary to get only the individual search result elements
 			.map(e => e.parentNode.parentNode.parentNode.parentNode) //el is the node for the div embodying the two divs that hold url/title and description
 			.map(el => ({
-				res: el.querySelectorAll("div div div span")[span].innerHTML.replace(/\ —\ /g, "").replaceAll(/<\/?span.*?>/g, ""), //removes emdash and spans
+				res: getres(el),
 				desc: getdesc(el),
 				title: el.childNodes[0].childNodes[0].childNodes[0].querySelector("h3").innerHTML,
 				url: el.childNodes[0].childNodes[0].childNodes[0].href,
-				imgurl: el.querySelector("g-img img").src,
+				imgurl: el.querySelector("g-img img") ? el.querySelector("g-img img").src : "No Image",
 			}))
 	})
-	callback(images);
+	callback(images, imageUrl);
 	await browser.close();
 });
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 const reverseImageSearch =  require('./index');
 
-const doSomething = (res) => {
-  console.log(res)
+const doSomething = (res, url) => {
+  console.log(url);
+  console.log(res);
+  console.log("-----------------------------------------------------------------------------------");
 }
 
-reverseImageSearch('https://cdn.discordapp.com/attachments/494223131683061763/868615421362839583/91403647_p0_master1200.png', doSomething)
+reverseImageSearch('https://media.discordapp.net/attachments/825057174178627614/876616859267530803/SPOILER_91929995_p0_master1200.png', doSomething);
+reverseImageSearch('https://media.discordapp.net/attachments/825057174178627614/876616913432756224/91752673_p0_master1200.png', doSomething);
